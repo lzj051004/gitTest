1e1c01c25c43dae67fd15e678c1dc7fa37fe55cd
lzj <617237820@qq.com>
2019-08-24 15:34:07 +0800

    branch_test

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
 
     @RequestMapping(value = "/test")
     public Object test(String str){
-        //tijiao 
+        //wqrwqiurowqi
         System.out.println("lzjbranch");
         return str;
     }
